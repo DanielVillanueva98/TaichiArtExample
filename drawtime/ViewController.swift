a9f60126830fc7b44edc6ba6bf3127ce36512430
Daniel Villanueva <dannyvillanueva200@gmail.com>
2021-01-11 16:07:04 -0800

    Initial Commit

---
+++ drawtime/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  drawtime
+//
+//  Created by King Villanueva on 1/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
